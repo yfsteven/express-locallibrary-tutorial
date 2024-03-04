d248fc5160bf98d91bc1d2279b9b5fec5788b11b
yfsteven <yaninquiry@gmail.com>
2024-03-03 19:35:44 -0500

    Updated files

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var createError = require('http-errors');
 var express = require('express');
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const dev_db_url = "mongodb+srv://Persona:BBQ8FIHU6BpdD3Qk@cluster0.vdvfz1q.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0";
 
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI
 
 main().catch((err) => console.log(err));
 async function main() {
